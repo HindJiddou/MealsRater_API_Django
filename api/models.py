b5da01faf20eedc74576caadacd558da83756e15
john doe <johndoe@example.com>
2024-09-23 15:27:41 +0000

    3_Token_User

--- api/models.py
+++ api/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.conf import settings
+from rest_framework.authtoken.models import Token
+
 
 
 class Meal(models.Model):
@@ -40,3 +45,10 @@ class Rating(models.Model):
         indexes = [
             models.Index(fields=['user', 'meal']),
         ]
+
+
+
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+# def TokenCreate(sender, instance, created, **kwargs):
+#     if created:
+#         Token.objects.create(user=instance)
